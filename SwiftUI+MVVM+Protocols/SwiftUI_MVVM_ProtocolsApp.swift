32af1d7aaa910524ad16c7845a9c4e97ddc21cba
Eren OLKUN <erenolkun@gmail.com>
2023-08-24 09:40:47 +0300

    Initial Commit

---
+++ SwiftUI+MVVM+Protocols/SwiftUI_MVVM_ProtocolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_ProtocolsApp.swift
+//  SwiftUI+MVVM+Protocols
+//
+//  Created by FERDA OLKUN on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_ProtocolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
